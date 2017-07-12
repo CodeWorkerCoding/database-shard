b1b9a03fb2e26a08223230fd68997a8cecfd7c4e
master-alen <nchufujianjian@126.com>
2017-07-12 18:21:00 +0800

    check in code

--- src/main/java/com/nchu/tech/database/config/MybatisConfig.java
+++ src/main/java/com/nchu/tech/database/config/MybatisConfig.java
@@ -1,10 +1,16 @@
 package com.nchu.tech.database.config;
 
+import org.apache.ibatis.mapping.DatabaseIdProvider;
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.mybatis.spring.boot.autoconfigure.MybatisProperties;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 import javax.annotation.Resource;
@@ -39,9 +45,10 @@ public class MybatisConfig extends MybatisAutoConfiguration {
         proxy.setDefaultTargetDataSource(masterDateSource);
         proxy.setTargetDataSources(targetDataResources);
 
-        proxy.afterPropertiesSet();
         return proxy;
     }
 
-
+    public MybatisConfig(MybatisProperties properties, ObjectProvider<Interceptor[]> interceptorsProvider, ResourceLoader resourceLoader, ObjectProvider<DatabaseIdProvider> databaseIdProvider) {
+        super(properties, interceptorsProvider, resourceLoader, databaseIdProvider);
+    }
 }
--- src/main/java/com/nchu/tech/database/config/ReaderWriteSpliteRoutingDataSource.java
+++ src/main/java/com/nchu/tech/database/config/ReaderWriteSpliteRoutingDataSource.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
  * Created by fujianjian on 2017/7/12.
  */
 public class ReaderWriteSpliteRoutingDataSource extends AbstractRoutingDataSource {
+
     @Override
     protected Object determineCurrentLookupKey() {
         return DataSourceContextHolder.getDataSourceType();
